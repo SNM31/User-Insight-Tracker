3d396e41e097eb9e2681e31c889c721d2d2aba66
SNM31 <anirudhmathur678@gmail.com>
2025-07-27 22:39:05 +0530

    c

--- api/src/main/java/com/anirudh/service/AnalyticsService.java
+++ api/src/main/java/com/anirudh/service/AnalyticsService.java
@@ -1,19 +1,22 @@
 package com.anirudh.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.anirudh.dto.AnalyticsResponse;
 import com.anirudh.dto.MetricsFilterRequest;
+import com.anirudh.repository.UserActivityRepository;
 
 @Service
 public class AnalyticsService {
     // This service will handle the business logic for analytics
     // It will interact with the database or any other data source to fetch metrics
+    private UserActivityRepository repository;
 
-    public AnalyticsResponse getMetrics(MetricsFilterRequest filterRequest) {
-        // Implement the logic to fetch and calculate metrics based on the filterRequest
-        // This is a placeholder implementation
-       
+    public AnalyticsResponse getAnalytics(LocalDateTime startDate, LocalDateTime endDate,
+                                                String region, String deviceType) {
+        return repository.getUserAnalytics(startDate, endDate, region, deviceType);
     }
     
 }
