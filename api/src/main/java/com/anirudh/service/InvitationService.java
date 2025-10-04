1a908ebdf864518579cd70112a442fc358a20a7f
SNM31 <anirudhmathur678@gmail.com>
2025-10-05 02:29:45 +0530

    invite token validation through redis

--- api/src/main/java/com/anirudh/service/InvitationService.java
+++ api/src/main/java/com/anirudh/service/InvitationService.java
@@ -1,6 +1,8 @@
 package com.anirudh.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.anirudh.dto.InvitationDto;
@@ -14,10 +16,27 @@ import java.util.UUID;
 public class InvitationService {
     @Autowired
     private JwtUtil jwtUtil;
+    @Autowired
+    @Qualifier("redisStringTemplate")
+    private RedisTemplate<String,String> invitationTokenMapping;
     public void sendInvite(InvitationDto inviteDetails)
     {
-        
-            String inviteLink="http://localhost:3000/register?token="+jwtUtil.generateInvitationToken(inviteDetails.getEmail(),"DASHBOARD_USER");
+            String invitationToken=jwtUtil.generateInvitationToken(inviteDetails.getEmail(),"DASHBOARD_USER");
+            invitationTokenMapping.opsForValue().set(invitationToken,inviteDetails.getEmail());
+            String inviteLink="http://localhost:3000/register?token="+invitationToken;
             System.out.println("Invite sent to: "+inviteDetails.getEmail()+" with link: "+inviteLink);
+    }
+    public void verifyInviteToken(String token)
+    {
+       try{
+         if(token==null || !invitationTokenMapping.hasKey(token))
+         {
+            throw new RuntimeException("Token is missing");
+         }
+         String email= jwtUtil.validateAndExtractEmail(token);
+       }catch(Exception e)
+       {
+        throw new RuntimeException("Invalid Token or Expired",e);
+       }
     }
 }
