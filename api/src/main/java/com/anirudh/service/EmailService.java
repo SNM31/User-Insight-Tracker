a994b7b017479a705c9c7a379b34e8f61238761f
SNM31 <anirudhmathur678@gmail.com>
2025-10-06 23:57:32 +0530

    email completed

--- api/src/main/java/com/anirudh/service/EmailService.java
+++ api/src/main/java/com/anirudh/service/EmailService.java
@@ -1,7 +1,10 @@
 package com.anirudh.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.resend.Resend;
 import com.resend.core.exception.ResendException;
@@ -12,15 +15,30 @@ import com.resend.services.emails.model.CreateEmailResponse;
 public class EmailService {
     @Value("${resend.api.key}")
     private String resendApiKey;
-    public void sendEmail(String to,String template)
+    @Autowired
+    private TemplateEngine templateEngine;
+ 
+     public void sendInvitationEmail(String to, String invitationLink) {
+    
+        Context context = new Context();
+        context.setVariable("invitationLink", invitationLink);
+
+        
+        String htmlContent = templateEngine.process("email/invitation-template", context);
+
+       
+        sendEmail(to, htmlContent);
+    }
+
+    private void sendEmail(String to,String htmlBody)
     {
         Resend resend=new Resend(resendApiKey);
-        System.out.println("Email sent to: "+to+" with template: "+template);
+        System.out.println("Email sent to: "+to+" with template: "+htmlBody);
         CreateEmailOptions emailOptions=CreateEmailOptions.builder()
         .from("19uec151@lnmmit.ac.in")
         .to(to)
         .subject("You're Invited to Join User Insight Tracker!")
-        .html(template)
+        .html(htmlBody)
         .build();
         try {
             CreateEmailResponse data = resend.emails().send(emailOptions);
--- api/src/main/java/com/anirudh/service/InvitationService.java
+++ api/src/main/java/com/anirudh/service/InvitationService.java
@@ -1,5 +1,6 @@
 package com.anirudh.service;
 
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -7,6 +8,10 @@ import org.springframework.stereotype.Service;
 
 import com.anirudh.dto.InvitationDto;
 import com.anirudh.utils.JwtUtil;
+import com.anirudh.utils.TokenGenerator;
+import com.resend.core.exception.ResendException;
+
+import ch.qos.logback.core.subst.Token;
 
 import java.util.Base64;
 import java.util.List;
@@ -19,24 +24,37 @@ public class InvitationService {
     @Autowired
     @Qualifier("redisStringTemplate")
     private RedisTemplate<String,String> invitationTokenMapping;
+    @Autowired
+    private EmailService emailService;
     public void sendInvite(InvitationDto inviteDetails)
     {
-            String invitationToken=jwtUtil.generateInvitationToken(inviteDetails.getEmail(),"DASHBOARD_USER");
+            String invitationToken=TokenGenerator.generateUniqueToken();
             invitationTokenMapping.opsForValue().set(invitationToken,inviteDetails.getEmail());
             String inviteLink="http://localhost:3000/register?token="+invitationToken;
             System.out.println("Invite sent to: "+inviteDetails.getEmail()+" with link: "+inviteLink);
+            try{
+                emailService.sendInvitationEmail(inviteDetails.getEmail(),inviteLink);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
     }
     public void verifyInviteToken(String token)
     {
-       try{
-         if(token==null || !invitationTokenMapping.hasKey(token))
-         {
-            throw new RuntimeException("Token is missing");
-         }
-         String email= jwtUtil.validateAndExtractEmail(token);
-       }catch(Exception e)
-       {
-        throw new RuntimeException("Invalid Token or Expired",e);
+      if (token == null || token.trim().isEmpty()) {
+        throw new IllegalArgumentException("Invitation token cannot be null or empty.");
        }
+       if(!invitationTokenMapping.hasKey(token))
+       {
+        throw new RuntimeException("Token not found or already used.");
+       }
+        String email = invitationTokenMapping.opsForValue().get(token);
+
+        if (email == null) {
+            // This case handles if your jwtUtil returns null for other validation failures
+            // (like a missing "scope" claim).
+            throw new RuntimeException("Token is not a valid invitation token.");
+        }
     }
 }
