cdd651942e3f66a981620542d7eb260d1f44b14c
SNM31 <anirudhmathur678@gmail.com>
2025-10-10 23:55:48 +0530

    google oauth provider for passing clientId

--- user-tracking-frontend/src/App.tsx
+++ user-tracking-frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { EventType, trackEvent } from './utils/tracker';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import AppRoutes from './AppRoutes';
 
 const App = () => {
+  const googleClientId=process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
   useEffect(() => {
     const handleUnload = () => {
       const loginTime = localStorage.getItem('loginTime');
@@ -20,9 +22,11 @@ const App = () => {
 
   return (
     <div className="App">
+      <GoogleOAuthProvider clientId={googleClientId}>
       <Router>
         <AppRoutes />
       </Router>
+      </GoogleOAuthProvider>
     </div>
   );
 };
