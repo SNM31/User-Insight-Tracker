ed4eba5b5497c9046ed7563f733e9d7f89ce3a82
SNM31 <anirudhmathur678@gmail.com>
2026-04-12 09:06:44 +0530

    c

--- api/src/main/java/com/anirudh/config/GoogleAuthConfig.java
+++ api/src/main/java/com/anirudh/config/GoogleAuthConfig.java
@@ -2,12 +2,14 @@ package com.anirudh.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.*;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import java.util.Collections;
 
+@Configuration
 public class GoogleAuthConfig {
      @Value("${spring.security.oauth2.client.registration.google.client-id}")
     private String googleClientId;
